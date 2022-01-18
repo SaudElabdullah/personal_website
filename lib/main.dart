482fd81b497ae7441758fc0e5438b26b7e7e51e2
SAWD ALABDALLAH <Soa142812222@gmail.com>
2022-01-18 21:34:03 +0300

    adding some information

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_website/screens/website.dart';
+import 'package:personal_website/utils/size_config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,23 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Personal Website',
-      home: Website(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Personal website',
+              home: Scaffold(
+                body: SafeArea(
+                  child: Website(),
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/screens/website.dart
+++ lib/screens/website.dart
@@ -1,4 +1,8 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:personal_website/utils/size_config.dart';
 
 class Website extends StatefulWidget {
   const Website({Key? key}) : super(key: key);
@@ -11,14 +15,152 @@ class _WebsiteState extends State<Website> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black.withOpacity(0.8),
-      body: SafeArea(
-        child: Stack(
-          alignment: AlignmentDirectional.topCenter,
-          children: [
-
-          ],
-        ),
+      backgroundColor: Colors.black.withOpacity(0.9),
+      body: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Positioned(
+            top: SizeConfig.heightMultiplier! * 3,
+            child: CircleAvatar(
+              radius: SizeConfig.widthMultiplier! * 15,
+              backgroundImage: const AssetImage(
+                'assets/myself.jpeg',
+              ),
+            ),
+          ),
+          Positioned(
+            top: SizeConfig.heightMultiplier! * 20,
+            child: DefaultTextStyle(
+              style: GoogleFonts.playfairDisplay(
+                textStyle: TextStyle(
+                  fontSize: SizeConfig.textMultiplier! * 2,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+              child: AnimatedTextKit(
+                isRepeatingAnimation: false,
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    'I am Saud Elabdullah',
+                    speed: const Duration(milliseconds: 150),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: SizeConfig.heightMultiplier! * 23,
+            child: Container(
+              padding: EdgeInsets.all(SizeConfig.widthMultiplier! * 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+              ),
+              child: Text(
+                'About \nA software engineer student at King Fahd University\n Flutter passionate and open-source supporter',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.playfairDisplay(
+                  textStyle: TextStyle(
+                    fontSize: SizeConfig.textMultiplier! * 1.3,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: SizeConfig.heightMultiplier! * 33,
+            left: SizeConfig.widthMultiplier! * 30,
+            child: Container(
+              padding: EdgeInsets.all(SizeConfig.widthMultiplier! * 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Projects',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.playfairDisplay(
+                      textStyle: TextStyle(
+                        fontSize: SizeConfig.textMultiplier! * 1.3,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    '1. Flutter budget planner 💵.\n2. Plant app UI 🌿.\n3. Spending tracker UI 💳.\n4. GlassMorphism Simple Example🧑‍💻.\nAnd Many other projects in my github repo 😉.',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.playfairDisplay(
+                      textStyle: TextStyle(
+                        fontSize: SizeConfig.textMultiplier! * 1.3,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: SizeConfig.heightMultiplier! * 36,
+            right: SizeConfig.widthMultiplier! * 30,
+            child: Container(
+              padding: EdgeInsets.all(SizeConfig.widthMultiplier! * 2),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+              ),
+              child: Row(
+                children: [
+                  Text(
+                    'vch',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.playfairDisplay(
+                      textStyle: TextStyle(
+                        fontSize: SizeConfig.textMultiplier! * 1.3,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: -math.pi / 4,
+                    child: Divider(
+                      height: SizeConfig.heightMultiplier! * 10,
+                      // indent: SizeConfig.heightMultiplier! * 1,
+                      // endIndent: SizeConfig.heightMultiplier! * 1,
+                      thickness: SizeConfig.heightMultiplier! * 10,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    'fhg',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.playfairDisplay(
+                      textStyle: TextStyle(
+                        fontSize: SizeConfig.textMultiplier! * 1.3,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
